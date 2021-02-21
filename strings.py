7f5ad1ed5fd86b1a5abbf06e70e58cfce9fa5b8e
Satriatzy <samachmad22@gmail.com>
2021-02-22 02:01:05 +0800

    Added Strings In Python

---
+++ strings.py
@@ -0,0 +1,28 @@
+###########################################
+# Learn Strings with Python
+# By Sam Achmad
+###########################################
+
+# This Code For Clear Terminal Screen
+import os
+os.system ('Clear')
+
+###################
+# Strings
+###################
+
+name = "SatriaTzy"
+greetings = "Hy, my name is  " + name
+
+print(greetings.upper())
+
+##################################
+#Learn Functions on Strings Python
+##################################
+
+#Functions Upper
+
+# Example2
+# greetings = "Hy, my name is Satria"
+
+#print(greetings)
