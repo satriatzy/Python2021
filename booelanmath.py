a448c29c5750d2ac7dba180e42fbaacb9600e0e8
Satriatzy <samachmad22@gmail.com>
2021-02-22 03:20:05 +0800

    Combination

--- booelanmath.py
+++ booelanmath.py
@@ -6,9 +6,21 @@
 # Function Float
 import os
 os.system('Clear')
-
-num = 10 # a Number 
+#Lecture 1
+#num = 10 # a Number 
 
 #print(num) when we start with call variable in the case num, so absolutely the identification show a int is 10
 # but when we put function float like below
-print(float(num))
+#print(float(num)) 
+
+#Lecture 2
+# Now we can get it started move with another diffrent think.
+# Combination with Math, Strings , integer and float
+
+num = "5" # in this case 5 its not int but its a string and we can do math in this strings 
+
+print(int(num) * 2) # so i use function int to made the result its 10 right ? cause that can be going to math lesson 
+					# its can diffrent answer if we just call num
+					# Please, u try print(num * 2) what u can see
+					# sure, the answer in ur bash show 55 or when we changed variable with another string like "Sam"
+					# absolutely the answer is samsam in ur bash 
