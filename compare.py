d9ecf958021b929e4fc4118c2151233dc293f890
SatriaTzy <45333174+satriatzy@users.noreply.github.com>
2021-02-22 14:55:17 +0800

    Add files via upload

---
+++ compare.py
@@ -0,0 +1,26 @@
+####################################
+# Learn Compare Operator in Python
+# By Satriatzy
+###################################
+
+import os
+os.system('Clear')
+
+# ==
+# !=
+# >
+# <
+# >=
+# <=
+
+#print( 200 == 119)
+
+#print( 100 != 100)
+
+#print(200 > 300)
+
+#print(100 < 99)
+
+#print(200 >= 100)
+
+print(100 <= 200)
