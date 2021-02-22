c2e6b1d9e1b83dacc8ce5d19f2fb1b2049c95a99
SatriaTzy <45333174+satriatzy@users.noreply.github.com>
2021-02-22 15:54:59 +0800

    Add files via upload

---
+++ multi.py
@@ -0,0 +1,24 @@
+#######################################
+# Learn Multiple Statement in Python
+# By Satriatzy
+#######################################
+
+import os
+os.system('Clear')
+
+# And 
+# Or
+
+num = 11
+
+#if (num > 10) and (num > 100) :
+	#print("Ur Number is Greater than 10 and Greater Than 100 !") # its mean when we use function and the condition both of that must be true
+																 # example when we use variable its number 11, u know that when we craete condition 
+																 # numb > 10 its mean yes thats true 11 more than 10 but not true 11 greater than 100
+																 # so automatic print in the bash cant show anything or its false
+
+# How about using function Or Like this below
+
+if (num > 10 ) or (num > 100) :
+	print("Ur Number Greater than 10 ")
+# its mean which one of that is true and then the bash will show 11 more than 10 
