224603f7d6b7c261ad2d211fa20e57fd70761162
SatriaTzy <45333174+satriatzy@users.noreply.github.com>
2021-02-22 11:23:29 +0800

    Add files via upload

---
+++ len.py
@@ -0,0 +1,17 @@
+##########################################
+# Learn Funtion Len and Combination with Strings and Math in Python
+# By Satriatzy
+##########################################
+
+# In this time we learn about function Len in python
+
+import os
+os.system('Clear')
+
+#First create a list
+
+names = ["Ibnu", "Rado", "Fairah"]
+
+# we add function len to know how many item in list 
+#print(len(names)) # Or we can find last list with print(names[len(names)-1])
+print(names[len(names)-1])
