9ac1f1b43fff36bb955ee892fe550a734d7a1184
SatriaTzy <45333174+satriatzy@users.noreply.github.com>
2021-02-22 11:14:03 +0800

    Add files via upload

---
+++ list.py
@@ -0,0 +1,25 @@
+###############################
+# Lear Lists On Python
+# By Satriatzy
+###############################
+
+import os
+os.system('Clear')
+
+# Learning Lists on python
+
+names = ["Abba", "Anita", "Bactiar"]
+
+names[0] = "Ilham"
+
+print(names) # in this case , first variable 3 list name and always remember always get it start 0 1 2 3 
+			 # The second variable want to start 0 with data type is string "Ilham"
+			 # result show 3 list name but Abba its not in the list 
+# Now We call function append to add more list and show of all
+
+#Second Variable its different like 
+# names.append("Ilham")
+# Now what Can be Happeb If i create different print just like below
+# print(names[3]) what can happened with that scrips when we hit or run
+# Absolutely the bash show Ilham as a 3. Cause Why ?
+# Remember when we add function append. that list have 4 name and pyton always count form 0 1 2 3 not begin 1
