f09ff8c7140fc49d1273fdcb29fbeb01afbaa355
SatriaTzy <45333174+satriatzy@users.noreply.github.com>
2021-02-22 16:21:55 +0800

    Add files via upload

---
+++ while.py
@@ -0,0 +1,16 @@
+#######################################
+# Learn While loops in Python
+# By Satriatzy
+#######################################
+
+import os
+
+os.system('Clear')
+
+# While Loops
+
+counter = 0
+
+while (counter <= 10):
+	print("The Count is: " + str(counter))
+	counter += 1	
