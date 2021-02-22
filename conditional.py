3256463acbc68ae433dff089c5de896edbd4a44e
SatriaTzy <45333174+satriatzy@users.noreply.github.com>
2021-02-22 15:17:40 +0800

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,19 @@
+#######################################
+# Learn Conditional Statement in Python
+# By Satriatzy
+#######################################
+
+import os
+os.system('Clear')
+
+# Conditional statement its mean we can do how we can compare two things from something need decision true or false to do
+# if / else / elif
+
+num = 9
+
+if (num > 10):
+	print("Ur Number Looks Great Than 10 ")
+elif(num == 10):
+	print("Ur Number is 10")
+else:
+	print("Oh no! Ur Number Under 10")
