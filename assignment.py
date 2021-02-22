62fdbb21b519f6c4a2d2d85cfc66f76a281de8e3
SatriaTzy <45333174+satriatzy@users.noreply.github.com>
2021-02-22 14:38:47 +0800

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,30 @@
+####################################
+# Learn Assignment Operator in Python
+# By Satriatzy
+###################################
+
+import os
+os.system('Clear')
+
+###############################
+# =
+# +
+# -=
+# *=
+# /=
+# **=
+# %=
+################################
+
+num = 300
+
+#num = num + 1
+
+#num *= 2 
+
+#num += 300
+
+#num -= 900
+
+print(num + 2) # we create something diffrents to make code more elegant 
+print(num)
